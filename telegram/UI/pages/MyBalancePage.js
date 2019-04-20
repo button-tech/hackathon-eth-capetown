02695cd942598ec0caa22a9fb986f8e2a0949768
Nick Kozlov <nk@buttonwallet.com>
2019-04-20 11:30:21 +0200

    add My Address / Balance pages

---
+++ telegram/UI/pages/MyBalancePage.js
@@ -0,0 +1,26 @@
+const db = require('../../../shared/db/db');
+const utils = require('../../utils/utils');
+const rp = require('request-promise');
+
+module.exports = {
+        setContext: function() {
+            return async (ctx) => {
+                const user = await db.user.find.oneByID(ctx.message.from.id);
+
+                const balanceEthRinkeby = await utils.web3Rinkeby.eth.getBalance(user.ethereumAddress);
+
+                const options = {
+                    method: 'GET',
+                    uri: `http://35.204.234.72:4228/eth/tokenBalance/0xef77ce798401dac8120f77dc2debd5455eddacf9/${user.ethereumAddress}`,
+                    json: true
+                };
+
+                const balanceDAI = await rp(options);
+                const ethBalanceInUsd = Number(await utils.course.convert("ETH", "USD", balanceEthRinkeby/1e18))
+                let msg = `*Your balance:* ${balanceETHMain/1e18} or ${(Number(await utils.course.convert("ETH", "USD", balanceEthRinkeby/1e18)))}$ \n\n
+                *DAI balance:* ${balanceDAI.balance}\n\n`;
+                ctx.reply(msg, { parse_mode: 'Markdown' });
+            };
+
+        },
+    };
