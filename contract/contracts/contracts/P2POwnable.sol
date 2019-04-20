7af8d00213f75d557dfeb69b4727cc468f4c9da1
Nick Kozlov <nk@buttonwallet.com>
2019-04-20 19:00:59 +0200

    update smart contract

--- contract/contracts/contracts/P2POwnable.sol
+++ contract/contracts/contracts/P2POwnable.sol
@@ -1,55 +1,58 @@
 pragma solidity >=0.5.2;
  
-import "https://raw.githubusercontent.com/OpenZeppelin/openzeppelin-solidity/master/contracts/ownership/Ownable.sol";
- 
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
 contract P2POwnable is Ownable {
-    uint constant BLOCK_EXPIRED_TIME = 5760;
-    uint constant MOVE_OWNER_LIMIT = 1e18;
-   
-    mapping(address => uint) public weight;
-    // request to update weights and protect from  
-    mapping (bytes32 => uint) public weightUpdateRequest;
-   
-   
-    event AddWeightUpdateRequest(bytes32 hash);
-    event RemoveWeightUpdateRequest(bytes32 hash);
-   
-    function addWeightUpdateRequest(bytes32 hash) public onlyOwner returns(bool) {
-        weightUpdateRequest[hash] = block.number;
-        emit AddWeightUpdateRequest(hash);
-        return true;
+  uint constant BLOCK_EXPIRED_TIME = 0 /* 5760  remove lag period for the hackathon*/;
+  uint constant MOVE_OWNER_LIMIT = 1e18;
+
+  mapping(address => uint) public weight;
+  // request to update weights and protect from
+  mapping (bytes32 => uint) public weightUpdateRequest;
+
+
+  event AddWeightUpdateRequest(bytes32 hash);
+  event RemoveWeightUpdateRequest(bytes32 hash);
+
+  function addWeightUpdateRequest(bytes32 hash) public onlyOwner returns(bool) {
+    weightUpdateRequest[hash] = block.number;
+    emit AddWeightUpdateRequest(hash);
+    return true;
+  }
+
+  function discardWeightUpdateRequest(bytes32 hash) public onlyOwner returns(bool) {
+    weightUpdateRequest[hash] = 0;
+    emit RemoveWeightUpdateRequest(hash);
+    return true;
+  }
+
+  function finalizeWeightUpdateRequest(bytes32 hash, address[] memory friends, uint[] memory weights) public onlyOwner returns(bool) {
+    require(weightUpdateRequest[hash] > 0);
+    require(block.number >= (weightUpdateRequest[hash] - BLOCK_EXPIRED_TIME));
+    require(friends.length == weights.length);
+    require(hash == keccak256(abi.encodePacked(friends, weights)));
+    for (uint i=0; i<friends.length; i++) {
+      weight[friends[i]] = weights[i];
     }
-   
-    function discardWeightUpdateRequest(bytes32 hash) public onlyOwner returns(bool) {
-        weightUpdateRequest[hash] = 0;
-        emit RemoveWeightUpdateRequest(hash);
-        return true;
+    return true;
+  }
+
+  function moveOwner(address newOwner, uint blockLimit, uint8[] memory v, bytes32[] memory r, bytes32[] memory s) public returns(bool) {
+    uint n = v.length;
+    require((n==r.length) &&(n==s.length));
+    require(block.number < blockLimit);
+    bytes32 message = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n52", newOwner, blockLimit));
+    uint totalWeight = 0;
+    address cursor = address(0);
+    for (uint i =0; i<n; i++){
+      address friend = ecrecover(message, v[i], r[i], s[i]);
+      require(cursor<friend);
+      cursor=friend;
+      totalWeight+=weight[friend];
     }
-   
-    function finalizeWeightUpdateRequest(bytes32 hash, address[] memory friends, uint[] memory weights) public onlyOwner returns(bool) {
-        require(weightUpdateRequest[hash] > 0);
-        require(weightUpdateRequest[hash] < (block.number - BLOCK_EXPIRED_TIME));
-        require(friends.length == weights.length);
-        require(hash == keccak256(abi.encodePacked(friends, weights)));
-        for (uint i=0; i<friends.length; i++) {
-            weight[friends[i]] = weights[i];
-        }
-        return true;
-    }
-   
-    function moveOwner(address newOwner, uint blockLimit, address[] memory friends, uint8[] memory v, bytes32[] memory r, bytes32[] memory s) public returns(bool) {
-        uint n = friends.length;
-        require((n== v.length) && (n==r.length) &&(n==s.length));
-        require(blockLimit < block.number);
-        bytes32 msg = keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n52", newOwner, blockLimit));
-        uint totalWeight = 0;
-        for (uint i =0; i<n; i++){
-            require(friends[i] == ecrecover(msg, v[i], r[i], s[i]));
-            totalWeight+=weight[friends[i]];
-        }
-        require(totalWeight>=MOVE_OWNER_LIMIT);
-        _transferOwnership(newOwner);
-        return true;
-    }
-   
+    require(totalWeight>=MOVE_OWNER_LIMIT);
+    _transferOwnership(newOwner);
+    return true;
+  }
+
 }
--- contract/contracts/contracts/Wallet.sol
+++ contract/contracts/contracts/Wallet.sol
@@ -4,39 +4,39 @@ pragma experimental ABIEncoderV2;
 import "./P2POwnable.sol";
  
 contract Wallet is P2POwnable {
-    function exec(address target, uint value, uint gasLimit, bytes memory data) public returns(bool success, bytes memory result) {
-        (success, result) = target.call.value(value).gas((gasLimit == 0) ? gasleft() : gasLimit)(data);
+  function exec(address target, uint value, uint gasLimit, bytes memory data) onlyOwner public  returns(bool success, bytes memory result) {
+    /* solium-disable-next-line */
+    (success, result) = target.call.value(value).gas((gasLimit == 0) ? gasleft() : gasLimit)(data);
+  }
+
+
+  event Log(bytes32 reqhash, bool success, bytes result);
+
+  function execMultipleAny(address[] memory target, uint[] memory value, uint[] memory gasLimit, bytes[] memory data) onlyOwner public payable returns(bool){
+    uint n = target.length;
+    require((value.length == n) && (gasLimit.length == n) && (data.length == n));
+    for (uint i=0; i<n; i++) {
+      bytes32 reqhash = keccak256(abi.encodePacked(target[i], value[i], gasLimit[i], data[i]));
+      /* solium-disable-next-line */
+      (bool success, bytes memory result) = target[i].call.value(value[i]).gas((gasLimit[i] == 0) ? gasleft() : gasLimit[i])(data[i]);
+      emit Log(reqhash, success, result);
     }
- 
-    function execView(address target, bytes memory data) public view returns(bool success, bytes memory result) {
-        (success, result) = target.staticcall(data);
+    return true;
+  }
+
+  function execMultipleAll(address[] memory target, uint[] memory value, uint[] memory gasLimit, bytes[] memory data) onlyOwner public payable returns(bool){
+    uint n = target.length;
+    require((value.length == n) && (gasLimit.length == n) && (data.length == n));
+    for (uint i=0; i<n; i++) {
+      bytes32 reqhash = keccak256(abi.encodePacked(target[i], value[i], gasLimit[i], data[i]));
+      /* solium-disable-next-line */
+      (bool success, bytes memory result) = target[i].call.value(value[i]).gas((gasLimit[i] == 0) ? gasleft() : gasLimit[i])(data[i]);
+      emit Log(reqhash, success, result);
+      require(success);
     }
-   
-    event Log(bytes32 reqhash, bool success, bytes result);
-   
-    function execMultipleAny(address[] memory target, uint[] memory value, uint[] memory gasLimit, bytes[] memory data) public returns(bool){
-        uint n = target.length;
-        require((value.length == n) && (gasLimit.length == n) && (data.length == n));
-        for (uint i=0; i<n; i++) {
-            bytes32 reqhash = keccak256(abi.encodePacked(target[i], value[i], gasLimit[i], data[i]));
-            (bool success, bytes memory result) = target[i].call.value(value[i]).gas((gasLimit[i] == 0) ? gasleft() : gasLimit[i])(data[i]);
-            emit Log(reqhash, success, result);
-        }
-        return true;
-    }
-   
-    function execMultipleAll(address[] memory target, uint[] memory value, uint[] memory gasLimit, bytes[] memory data) public returns(bool){
-        uint n = target.length;
-        require((value.length == n) && (gasLimit.length == n) && (data.length == n));
-        for (uint i=0; i<n; i++) {
-            bytes32 reqhash = keccak256(abi.encodePacked(target[i], value[i], gasLimit[i], data[i]));
-            (bool success, bytes memory result) = target[i].call.value(value[i]).gas((gasLimit[i] == 0) ? gasleft() : gasLimit[i])(data[i]);
-            emit Log(reqhash, success, result);
-            require(success);
-        }
-        return true;
-    }
-   
-    function() external payable {}
- 
+    return true;
+  }
+
+  function() external payable {}
+
 }
