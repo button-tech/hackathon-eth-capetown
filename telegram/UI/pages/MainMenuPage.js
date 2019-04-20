91ca15882ac4a34eb2cd12f07ab8c157b7c07472
artem <artall64@gmail.com>
2019-04-20 23:49:37 +0200

    main menu fix

--- telegram/UI/pages/MainMenuPage.js
+++ telegram/UI/pages/MainMenuPage.js
@@ -5,23 +5,18 @@ const Text = require('../../../shared/text');
 const db = require('../../../shared/db/db');
 
 module.exports = {
-        setContext: function() {
+    setContext: function () {
 
-            PageConstructor.setKeyboardButtonPageCallback(Text.keyboard.start.button["0"], "Account");
-            PageConstructor.setKeyboardButtonPageCallback(Text.keyboard.start.button["1"], "Wallet");
+        PageConstructor.setKeyboardButtonPageCallback(Text.keyboard.start.button["0"], "Account");
+        PageConstructor.setKeyboardButtonPageCallback(Text.keyboard.start.button["1"], "Wallet");
 
-            return async (ctx) => {
-                const user = await db.user.find.oneByID(ctx.message ? ctx.message.from.id : ctx.update.callback_query.from.id);
-                if (user) {
-                    const keyboard = user.isRegistredForBackup ? Keyboard.startRegistredForBackup : Keyboard.start;
-                    return ctx.reply(Text.keyboard.start.text, Markup
-                        .keyboard(keyboard)
-                        .resize()
-                        .extra()
-                    );
-                }
-                else
-                    return PageConstructor.render("CreateAccount")(ctx)
-            };
-        },
-    };
+        return async (ctx) => {
+            const user = await db.user.find.oneByID(ctx.message ? ctx.message.from.id : ctx.update.callback_query.from.id);
+            if (!user)
+                return PageConstructor.render("CreateAccount")(ctx);
+            
+            const msg = Markup.keyboard(Keyboard.start).resize().extra();
+            return ctx.reply(Text.keyboard.start.text, msg);
+        };
+    },
+};
