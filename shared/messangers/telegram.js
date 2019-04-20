d35d020c0b3e763341a00b8fc623c10491cd0fa1
Nick Kozlov <nk@buttonwallet.com>
2019-04-20 09:54:01 +0200

    add messengers / notifiactions x2

--- shared/messangers/telegram.js
+++ shared/messangers/telegram.js
@@ -19,6 +19,47 @@ const sendMessage = (userID, keyboard, text) => {
     return rp(options);
 };
 
+const sendInlineButton = (userID, text, urlButtonText, url) => {
+    const options = {
+        method: 'POST',
+        uri: `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+        body: {
+            'chat_id': userID,
+            'text': text,
+            'parse_mode': 'Markdown',
+            'reply_markup': JSON.stringify({
+                "inline_keyboard": [[{
+                    "text": urlButtonText,
+                    "url": url,
+                }]],
+            }),
+            'disable_web_page_preview': true
+        },
+        json: true
+    };
+    return rp(options);
+};
+
+const sendInlineButtonCallbackType = (userID, text, buttonText, callback) => {
+    const options = {
+        method: 'POST',
+        uri: `https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`,
+        body: {
+            'chat_id': userID,
+            'text': text,
+            'parse_mode': 'Markdown',
+            'reply_markup': JSON.stringify({
+                "inline_keyboard": [[{
+                    "text": text,
+                    "callback": callback,
+                }]],
+            }),
+            'disable_web_page_preview': true
+        },
+        json: true
+    };
+    return rp(options);
+};
 
 module.exports = {
     sendMessage: sendMessage,
