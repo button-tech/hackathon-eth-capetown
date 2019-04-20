3d45128ffaeed17c83a156b52fccd4640de7ec82
Nick Kozlov <nk@buttonwallet.com>
2019-04-20 10:36:42 +0200

    init db all

--- shared/db/db.js
+++ shared/db/db.js
@@ -19,11 +19,106 @@ const user = {
                 });
             });
         },
+        oneByID: (userID) => {
+            return new Promise((resolve, reject) => {
+                User.findOne({userID: Number(userID)}, (err, doc) => {
+                    if (err) {
+                        reject(err);
+                        return;
+                    }
+                    resolve(doc);
+                });
+            });
+        },
+        oneByNickname: (nickname) => {
+            return new Promise((resolve, reject) => {
+                User.find({nickname: new RegExp(nickname)}, (err, doc) => {
+                    if (err)
+                        reject(err);
+                    resolve(doc[0]);
+                });
+            })
+        }
+    },
+    update: {
+        addresses: (userID, ethereumAddress, bitcoinAddress) => {
+            return new Promise((resolve, reject) => {
+                User.updateOne({userID: userID}, {ethereumAddress: ethereumAddress, bitcoinAddress: bitcoinAddress}, (err, doc) => {
+                    if (err)
+                        reject(err);
+                    resolve(doc);
+                });
+            });
+        },
+        tokenAddresses: async (userID, tokenAddress) => {
+            User.find({userID: userID}, (err, doc) => {
+                if (err)
+                    return err;
+                const tokens = doc[0].tokenAddresses;
+                tokens.push(tokenAddress);
+                return new Promise((resolve, reject) => {
+                    User.update({userID: userID}, {tokenAddresses: tokens}, (err, doc) => {
+                        if (err)
+                            reject(err);
+                        resolve(doc);
+                    });
+                });
+            });
 
+        },
+        friendsForRestore: async (userID, friends)=>{
+            return new Promise((resolve, reject) => {
+                User.updateOne({userID: userID}, {friendsForRestore: friends}, (err, doc) => {
+                    if (err)
+                        reject(err);
+                    resolve(doc);
+                });
+            });
+        },
+        registred: async (userID) => {
+            return new Promise((resolve, reject) => {
+                User.updateOne({userID: userID}, {isRegistredForBackup: true}, (err, doc) => {
+                    if (err)
+                        reject(err);
+                    resolve(doc);
+                });
+            });
+        }
     }
 };
 
-
+const transaction = {
+    create: async (currency, fromUserID, toUserID, toAddress, amount, amountInUSD, txHash) => Transaction.create({
+        currency: currency,
+        fromUserID: fromUserID,
+        toUserID: toUserID,
+        toAddress: toAddress,
+        amount: amount,
+        amountInUSD: amountInUSD,
+        txHash: txHash
+    }, (err, doc) => {
+    }),
+    find: {
+        toUserID: (userID) => {
+            return new Promise((resolve, reject) => {
+                Transaction.find({toUserID: userID}, (err, doc) => {
+                    if (err)
+                        reject(err);
+                    resolve(doc[0]);
+                });
+            });
+        },
+        fromUserID: (userID) => {
+            return new Promise((resolve, reject) => {
+                Transaction.find({fromUserID: userID}, (err, doc) => {
+                    if (err)
+                        reject(err);
+                    resolve(doc[0]);
+                });
+            });
+        }
+    }
+};
 
 
 module.exports = {
